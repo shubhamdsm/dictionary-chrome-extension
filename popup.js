2cc1f9703c545611ab20016a63c71b2550d559aa
shubham <shubhamdv19@gmail.com>
2021-08-17 22:08:36 +0530

    final commit

--- popup.js
+++ popup.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    document.getElementById('search').addEventListener('click', function(e){
+    document.getElementById('search').addEventListener('click', function (e) {
         e.preventDefault();
         const word = document.getElementById('word').value;
-        if(word === ""){
+        if (word === "") {
             document.getElementById('error').innerText = `Please enter something`;
-        } else{
-          document.getElementById('error').innerText = '';     
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
-        fetch(url)
-        .then(res => res.json())
-        .then(data => renderResults(data))
-        .catch(error => handleError(error))
-     } })
+        } else {
+            document.getElementById('error').innerText = '';
+            const url = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
+            fetch(url)
+                .then(res => res.json())
+                .then(data => renderResults(data))
+                .catch(error => handleError(error))
+        }
+    })
 })
 
-function renderResults(data){
-        data.forEach(element => {
+function renderResults(data) {
+    data.forEach(element => {
         document.getElementById('content').innerText = `Your word : ${element['word']}`
         const arr = Object.values(element['meanings'])
         const arrofdefinitons = arr[0]['definitions']
         const singleDefin = arrofdefinitons[0].definition;
         document.getElementById('definition').innerText = `Definition: ${singleDefin}`;
-        
+
+
     });
 }
-function handleError(){
+
+function handleError() {
     document.getElementById('error').innerText = `No definition found `
 }
